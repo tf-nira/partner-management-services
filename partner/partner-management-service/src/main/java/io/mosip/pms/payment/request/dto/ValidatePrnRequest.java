c7e463d470eea4892b2ada5244268566bac87411
jagadeesh.t <jagadeesh.t@TECHNO-566.technoforte.co.in>
2026-01-12 16:09:56 +0530

    Added ValidatePrn

---
+++ partner/partner-management-service/src/main/java/io/mosip/pms/payment/request/dto/ValidatePrnRequest.java
@@ -0,0 +1,10 @@
+package io.mosip.pms.payment.request.dto;
+
+import lombok.Data;
+
+@Data
+public class ValidatePrnRequest {
+
+    public String prn;
+
+}
--- partner/partner-management-service/src/main/java/io/mosip/pms/payment/service/PaymentService.java
+++ partner/partner-management-service/src/main/java/io/mosip/pms/payment/service/PaymentService.java
@@ -1,11 +1,13 @@
 package io.mosip.pms.payment.service;
 
 import io.mosip.pms.payment.request.dto.PrnRequest;
+import io.mosip.pms.payment.request.dto.ValidatePrnRequest;
 import io.mosip.pms.payment.response.dto.PrnResponse;
+import io.mosip.pms.payment.response.dto.ValidatePrnResponse;
 
 public interface PaymentService {
 
     public PrnResponse generatePrn(PrnRequest request);
 
-    public PrnResponse validatePrn(PrnRequest request);
+    public ValidatePrnResponse validatePrn(ValidatePrnRequest request);
 }
