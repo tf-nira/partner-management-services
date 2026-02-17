92a92d511aebf8928f639cb11e8b73ae5b628f81
Karthik <karthickktk2003@gmail.com>
2026-02-17 15:01:29 +0530

    pmpdev

--- partner/pms-common/src/main/java/io/mosip/pms/common/helper/WebSubPublisher.java
+++ partner/pms-common/src/main/java/io/mosip/pms/common/helper/WebSubPublisher.java
@@ -3,6 +3,11 @@ package io.mosip.pms.common.helper;
 import java.util.Map;
 import java.util.UUID;
 
+import io.mosip.kernel.core.websub.spi.SubscriptionClient;
+import io.mosip.kernel.websub.api.exception.WebSubClientException;
+import io.mosip.kernel.websub.api.model.SubscriptionChangeRequest;
+import io.mosip.kernel.websub.api.model.SubscriptionChangeResponse;
+import io.mosip.kernel.websub.api.model.UnsubscriptionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +21,7 @@ import io.mosip.pms.common.dto.Event;
 import io.mosip.pms.common.dto.EventModel;
 import io.mosip.pms.common.dto.Type;
 import io.mosip.pms.common.util.PMSLogger;
+import org.springframework.scheduling.annotation.Scheduled;
 
 public class WebSubPublisher {
 
@@ -26,6 +32,54 @@ public class WebSubPublisher {
 	
 	@Autowired
 	private PublisherClient<String, EventModel, HttpHeaders> pb;
+
+	@Autowired
+	private PublisherClient<String, Object, HttpHeaders> pbObj;
+
+	@Value("${websub.hub.url}")
+	private String hubURL;
+	@Value("${partner-websub-ida-partner-service-callback-secret}")
+	private String callbackSecret;
+	@Value("${pms.websub.topic.partner.amount.updated}")
+	private String topic;
+	@Value("${pms-websub-partner-service-partner-amount-updated-callback-relative-url}")
+	private String callbackUrl;
+
+	@Autowired
+	protected SubscriptionClient<
+			SubscriptionChangeRequest,
+			UnsubscriptionRequest,
+			SubscriptionChangeResponse> subscriptionClient;
+
+	@Scheduled(fixedDelayString = "${websub.resubscribe.interval}",
+			initialDelayString = "${websub.event.delay-millisecs}")
+	public void initSubsriptions() {
+		logger.info("Initializing subscribptions... ");
+		subscribeForSettledEvents();
+	}
+
+	public void subscribeForSettledEvents() {
+		try {
+			SubscriptionChangeRequest subscriptionRequest = new SubscriptionChangeRequest();
+			subscriptionRequest.setCallbackURL(callbackUrl);
+			subscriptionRequest.setHubURL(hubURL);
+			subscriptionRequest.setSecret(callbackSecret);
+			subscriptionRequest.setTopic(topic);
+			logger.info("subscribing... ");
+			subscriptionClient.subscribe(subscriptionRequest);
+		} catch (WebSubClientException e) {
+			logger.info("error in subscribing... ");
+		}
+	}
+
+	public void registerForSettledEvents() {
+		try {
+			pbObj.registerTopic(topic, webSubHubPublishUrl);
+		} catch (WebSubClientException e) {
+			logger.info("topic already registered");
+		}
+
+	}
 	
 	public void notify(EventType eventType,Map<String,Object> data,Type type) {
 		sendEventToIDA(createEventModel(eventType,data,type));
