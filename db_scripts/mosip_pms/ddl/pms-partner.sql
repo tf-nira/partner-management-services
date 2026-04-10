cb77ca405502e284ce75d9e4a8e88a5b649a3cc6
Karthik <karthickktk2003@gmail.com>
2026-04-10 08:38:31 +0530

    pmpdev

--- db_scripts/mosip_pms/ddl/pms-partner.sql
+++ db_scripts/mosip_pms/ddl/pms-partner.sql
@@ -21,6 +21,8 @@ CREATE TABLE pms.partner(
 	logo_url character varying(256),
 	addl_info character varying,
 	requires_payment boolean NOT NULL,
+	partnerAuthType character varying(128),
+	partnerGroup character varying(128),
     CONSTRAINT pk_part PRIMARY KEY (id)
 );
 
--- db_upgrade_scripts/mosip_pms/sql/nira_to_payment_rollback.sql
+++ db_upgrade_scripts/mosip_pms/sql/nira_to_payment_rollback.sql
@@ -2,7 +2,17 @@ DROP TABLE IF EXISTS pms.partner_prn;
 DROP TABLE IF EXISTS pms.partner_current_balance;
 DROP TABLE IF EXISTS pms.partner_payment_transactions;
 DROP TABLE IF EXISTS pms.notification_history;
+DROP TABLE IF EXISTS pms.partners_transaction;
 
 
-ALTER TABLE pms.partner DROP COLUMN requires_payment;
-ALTER TABLE pms.partner_h DROP COLUMN requires_payment;
+-- partner table
+ALTER TABLE pms.partner 
+DROP COLUMN IF EXISTS requires_payment,
+ADD COLUMN IF NOT EXISTS partnerAuthType character varying(128),
+ADD COLUMN IF NOT EXISTS partnerGroup character varying(128);
+
+-- partner_h table
+ALTER TABLE pms.partner_h 
+DROP COLUMN IF EXISTS requires_payment,
+ADD COLUMN IF NOT EXISTS partnerAuthType character varying(128),
+ADD COLUMN IF NOT EXISTS partnerGroup character varying(128);
